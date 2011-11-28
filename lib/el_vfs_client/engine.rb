38e9b5d4f084d758ffc46ec6fa19e9d8e6b231a0
Manin Iliy <bkmzvf@gmail.com>
2011-11-28 17:25:23 +0700

    hot bugfix

--- lib/el_vfs_client/engine.rb
+++ lib/el_vfs_client/engine.rb
@@ -3,7 +3,7 @@ module ElVfsClient
     isolate_namespace ElVfsClient
     engine_root = File.expand_path("../../../", __FILE__)
     config.after_initialize do
-      raise 'Please specify url for El VFS in settings yml!' if defined?(Settings) && Settings[:el_vfs]
+      raise 'Please specify url for El VFS in settings yml!' unless defined?(Settings) && Settings[:el_vfs]
     end
   end
 end
