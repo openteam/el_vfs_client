951d695b2b7c125652809cdc6e5b62da944efbb8
Dmitry Lihachev <lda@openteam.ru>
2011-12-16 09:32:03 +0700

    read VFS_URL instead of VFS_HOST

--- lib/el_vfs_client/engine.rb
+++ lib/el_vfs_client/engine.rb
@@ -4,13 +4,7 @@ module ElVfsClient
     engine_root = File.expand_path("../../../", __FILE__)
     config.after_initialize do
       require 'configliere'
-      Settings.define 'vfs.url', :env_var => 'VFS_HOST', :required => true
-      if Settings['el_vfs']
-        Settings['vfs.url'] = "#{Settings[:el_vfs][:protocol]}://#{Settings[:el_vfs][:host]}:#{Settings[:el_vfs][:port]}"
-        puts
-        puts "!!! WARNING !!! Use vfs.host instead el_vfs.protocol, el_vfs.host, el_vfs.port."
-        puts
-      end
+      Settings.define 'vfs.url', :env_var => 'VFS_URL', :required => true
       Settings.resolve!
     end
   end
