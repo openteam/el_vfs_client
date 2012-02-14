19feb770b2f08b7b3a95817731aa7879f78c166a
Manin Iliy <bkmzvf@gmail.com>
2012-02-14 12:28:01 +0700

    fix assets

--- lib/el_vfs_client.rb
+++ lib/el_vfs_client.rb
@@ -1,4 +1,10 @@
 require "el_vfs_client/engine"
+require 'rails'
 
 module ElVfsClient
+  class Engine < ::Rails::Engine
+    initializer :assets do |config|
+      ::Rails.application.config.assets.precompile += %w( engine.js el_vfs/el_vfs.css )
+    end
+  end
 end
