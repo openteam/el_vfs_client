4c87429647bb60436c919305a28cadf79f98bbec
Evgeny Lapin <lev@openteam.ru>
2014-04-29 09:45:10 +0700

    added route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ module ElVfsClient
   Engine.routes.draw do
     get '/el_finder' => 'el_finder#show'
     get '/api/el_finder/v2' => 'el_finder#run'
+    post '/api/el_finder/v2' => 'el_finder#run'
   end
 end
